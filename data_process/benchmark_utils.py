01013f80a8e50d7cf8fdac7640dbf6db8196f231
yankang <kangyan2003@gmail.com>
2021-05-07 16:10:31 +0800

    add support for save benchmark result and record hyperparameters for best metrics

--- data_process/benchmark_utils.py
+++ data_process/benchmark_utils.py
@@ -4,6 +4,8 @@ from lightgbm.sklearn import LGBMClassifier
 from scipy.stats import ks_2samp
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, roc_auc_score
+import os
+import json
 
 
 def show_metrics(test_label, pred_label, pred_prob_label):
@@ -11,7 +13,7 @@ def show_metrics(test_label, pred_label, pred_prob_label):
     print("pred_label:", pred_label.shape, type(pred_label), sum(test_label))
     print("pred_prob_label:", pred_prob_label.shape, type(pred_prob_label), sum(pred_prob_label))
     get_ks = lambda y_pred, y_true: ks_2samp(y_pred[y_true == 1], y_pred[y_true != 1]).statistic
-    ks_test = get_ks(np.array(pred_prob_label[:, 1]), np.array(test_label))
+    ks = get_ks(np.array(pred_prob_label[:, 1]), np.array(test_label))
 
     print("test_label shape:", test_label.shape)
     print("pred_label shape:", pred_label.shape)
@@ -19,14 +21,23 @@ def show_metrics(test_label, pred_label, pred_prob_label):
     # auc1 = roc_auc_score(test_label, pred_label)
     auc = roc_auc_score(test_label, pred_prob_label[:, 1])
     print(f"accuracy : {acc}")
-    print(f"ks_test : {ks_test}")
-    # print(f"auc1 : {auc1}")
     print(f"auc : {auc}")
+    print(f"ks: {ks}")
+    return acc, auc, ks
+
+
+def find_args_for_best_metric(result_list, model_name='xgb', metric_name='auc'):
+    best_metric = 0.0
+    best_arg = None
+    for result in result_list:
+        tmp_auc = result[1][model_name][metric_name]
+        if tmp_auc > best_metric:
+            best_metric = tmp_auc
+            best_arg = result[0]
+    return best_metric, best_arg
 
 
 def prepare_models(kwargs):
-    model_list = list()
-
     n_tree_estimators = kwargs.get('n_tree_estimators')
     max_depth = kwargs.get('max_depth')
     n_tree_estimators = 200 if n_tree_estimators is None else n_tree_estimators
@@ -37,29 +48,35 @@ def prepare_models(kwargs):
                   'feature_fraction': 0.7, 'bagging_fraction': 0.72, 'bagging_freq': 3, 'max_depth': max_depth,
                   'min_child_samples': 10}
 
-    xgb_params = {'booster': 'gbtree', 'lambda': 10.0, 'alpha': 0.001, 'n_estimators': n_tree_estimators,
-                  'eta': 0.03, 'gamma': 0.34, 'grow_policy': 'lossguide', 'max_depth': max_depth,
-                  'feature_fraction': 0.7, 'bagging_fraction': 0.72, 'bagging_freq': 3, 'min_child_samples': 20}
+    xgb_params = {'booster': 'gbtree', 'lambda': 0.01, 'alpha': 0.01, 'n_estimators': n_tree_estimators,
+                  'eta': 0.05, 'gamma': 0.34, 'grow_policy': 'lossguide', 'max_depth': max_depth,
+                  'feature_fraction': 0.7, 'bagging_fraction': 0.72, 'bagging_freq': 3, 'min_child_samples': 10}
 
-    model_list.append(LogisticRegression(max_iter=500, solver="sag"))
-    model_list.append(LGBMClassifier(**lgb_params))
-    model_list.append(xgb.XGBClassifier(**xgb_params))
+    model_dict = {
+        "lr": LogisticRegression(),
+        # "lgbm": LGBMClassifier(**lgb_params),
+        "xgb": xgb.XGBClassifier(**xgb_params)}
 
-    return model_list
+    return model_dict
 
 
 def run_benchmark(train_data, train_label, test_data, test_label, **kwargs):
-    print(f"[INFO] train_data shape:{train_data.shape}")
-    print(f"[INFO] train_label shape:{train_label.shape}， {np.sum(train_label)}")
-    print(f"[INFO] test_data shape:{test_data.shape}")
-    print(f"[INFO] test_label shape:{test_label.shape}， {np.sum(test_label)}")
-
-    for model in prepare_models(kwargs):
+    result_dict = dict()
+    for model_name, model in prepare_models(kwargs).items():
         print("-" * 100)
         print(f"==> model:{model}")
         # trained_model = model.fit(B_train_data, B_train_label)
         trained_model = model.fit(train_data, train_label)
-        B_pred_label = trained_model.predict(test_data)
-        B_pred_prob_label = trained_model.predict_proba(test_data)
+        pred_label = trained_model.predict(test_data)
+        pred_prob_label = trained_model.predict_proba(test_data)
         test_label = test_label.flatten()
-        show_metrics(test_label, B_pred_label, B_pred_prob_label)
+        acc, auc, ks = show_metrics(test_label, pred_label, pred_prob_label)
+        result_dict[model_name] = {"acc": acc, "auc": auc, "ks": ks}
+    return result_dict
+
+
+def save_benchmark_result(result, to_dir, file_name):
+    file_full_name = os.path.join(to_dir, file_name + ".json")
+    with open(file_full_name, 'w') as outfile:
+        json.dump(result, outfile)
+    print(f"[INFO] save benchmark result to {file_full_name}")
