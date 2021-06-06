182a30ff05d36277b1d1aa4dd89ad65fa23e435b
yankang <kangyan2003@gmail.com>
2021-06-06 21:25:05 +0800

    Add func for drawing feature distribution

--- utils.py
+++ utils.py
@@ -3,10 +3,13 @@ import os
 from datetime import date
 from datetime import datetime
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.stats import ks_2samp
 from sklearn.metrics import roc_auc_score, accuracy_score
+
+from statistics_utils import kl_divergence
 
 
 def get_latest_timestamp(timestamped_file_name, folder):
@@ -71,7 +74,7 @@ def load_dann_experiment_result(root, task_id, timestamp=None):
     return dann_exp_result_dict
 
 
-def test_classification(model, data_loader, tag):
+def test_classifier(model, data_loader, tag, use_src_classifier=True):
     print(f"---------- {tag} classification ----------")
     correct = 0
     n_total = 0
@@ -82,7 +85,9 @@ def test_classification(model, data_loader, tag):
     for batch_idx, (data, label) in enumerate(data_loader):
         label = label.flatten()
         n_total += len(label)
-        batch_corr, y_pred, pos_y_prob = model.calculate_classifier_correctness(data, label)
+        batch_corr, y_pred, pos_y_prob = model.calculate_classifier_correctness(data,
+                                                                                label,
+                                                                                use_source_classifier=use_src_classifier)
         correct += batch_corr
         y_real_list += label.tolist()
         y_pred_list += y_pred.tolist()
@@ -142,15 +147,28 @@ def test_discriminator(model, num_regions, source_loader, target_loader):
     print(f"[DEBUG] {n_source_total} source domain acc: {source_acc}, mean: {ave_source_acc}")
     print(f"[DEBUG] {n_target_total} target domain acc: {target_acc}, mean: {ave_target_acc}")
     print(f"[DEBUG] total domain acc: {total_acc}, mean: {ave_total_acc}")
-    # entropy_domain_acc = entropy(cat_acc / acc_sum)
-    # print(f"[DEBUG] domain acc entropy: {entropy_domain_acc}")
-    return (ave_total_acc, ave_source_acc, ave_target_acc), (list(total_acc), list(source_acc), list(target_acc))
+    entropy_domain_acc = entropy(cat_acc / acc_sum)
+    ave_entropy = np.mean(entropy_domain_acc)
+    print(f"[DEBUG] domain acc entropy: {entropy_domain_acc}, mean:{ave_entropy} ")
+    return (ave_total_acc, ave_source_acc, ave_target_acc), (list(total_acc), list(source_acc), list(target_acc)), ave_entropy
 
 
 def entropy(predictions):
     epsilon = 1e-6
     H = -predictions * np.log2(predictions + epsilon)
     return H.sum(axis=0)
+
+
+def compute(auc_list, ks_list, tag, top_k=5):
+    print("-" * 150)
+    ppd_fg_sum = np.array(auc_list) + np.array(ks_list)
+    sorted_result = np.argsort(ppd_fg_sum)
+    ppd_fg_top5_auc = auc_list[sorted_result[-top_k:]]
+    ppd_fg_top5_ks = ks_list[sorted_result[-top_k:]]
+    print("[INFO] {}_top5_auc:{}, mean:{}, std:{}".format(tag, ppd_fg_top5_auc, np.mean(ppd_fg_top5_auc),
+                                                          np.std(ppd_fg_top5_auc)))
+    print("[INFO] {}_top5_ks:{}, mean:{}, std:{}".format(tag, ppd_fg_top5_ks, np.mean(ppd_fg_top5_ks),
+                                                         np.std(ppd_fg_top5_ks)))
 
 
 def produce_data_for_lr_shap(model, data_loader, column_name_list, output_file_full_name):
@@ -172,3 +190,66 @@ def produce_data_for_lr_shap(model, data_loader, column_name_list, output_file_f
     print(df_lr_input.head(5))
     df_lr_input.to_csv(output_file_full_name, index=False)
     print(f"[INFO] save lr-data-shap to {output_file_full_name}")
+
+
+def produce_data_for_distribution(model,
+                                  src_train_loader,
+                                  tgt_train_loader,
+                                  feature_group_name_list,
+                                  to_dir,
+                                  tag):
+    src_train_iter = iter(src_train_loader)
+    tgt_train_iter = iter(tgt_train_loader)
+
+    src_data, _ = src_train_iter.next()
+    tgt_data, _ = tgt_train_iter.next()
+
+    src_fg_emb_list = model.calculate_feature_group_embedding_list(src_data)
+    tgt_fg_emb_list = model.calculate_feature_group_embedding_list(tgt_data)
+
+    for src_fg_emb, tgt_fg_emb, name in zip(src_fg_emb_list, tgt_fg_emb_list, feature_group_name_list):
+        df_src_data = pd.DataFrame(data=src_fg_emb.detach().numpy())
+        df_tgt_data = pd.DataFrame(data=tgt_fg_emb.detach().numpy())
+        file_fill_name = to_dir + "/prada_{}_src_emb_{}.csv".format(tag, name)
+        df_src_data.to_csv(file_fill_name, header=False, index=False)
+        print(f"save df_src_data to {file_fill_name} with shape {df_src_data.shape}")
+
+        file_fill_name = to_dir + "/prada_{}_tgt_emb_{}.csv".format(tag, name)
+        df_tgt_data.to_csv(file_fill_name, header=False, index=False)
+        print(f"save df_tgt_data to {file_fill_name} with shape {df_tgt_data.shape}")
+
+
+def compute_kl_divergence(src_data, tgt_data, n_components):
+    kl = 0.0
+    for idx in range(n_components):
+        kl += kl_divergence(tgt_data[:, idx], src_data[:, idx])
+    kl = kl / n_components
+    print(f"[INFO] KL-divergence: {kl}")
+    return kl
+
+
+def draw_distribution(df_src_data, df_tgt_data, num_points, dim_reducer, tag, feature_group_name, to_dir):
+    print("[INFO] draw distribution")
+
+    df_src_tgt_data = pd.concat([df_src_data, df_tgt_data], axis=0)
+    num_src = num_points if df_src_data.shape[0] > num_points else df_src_data.shape[0]
+
+    all_data = dim_reducer.fit_transform(df_src_tgt_data.values)
+
+    # TODO: save all data
+    print(f"[INFO] src_data shape:{df_src_data.shape}")
+    print(f"[INFO] tgt_data shape:{df_tgt_data.shape}")
+    print(f"[INFO] all_data shape:{all_data.shape}")
+
+    # estimate KL divergence
+    kl = compute_kl_divergence(all_data[:num_src], all_data[-num_src:], all_data.shape[1])
+
+    plt.figure(figsize=(8, 8))
+    plt.scatter(all_data[:num_src, 0], all_data[:num_src, 1], s=10, c='r')
+    plt.scatter(all_data[-num_src:, 0], all_data[-num_src:, 1], s=10, c='b')
+    plt.title(tag + "-" + feature_group_name)
+    plt.xlabel(f"KL={kl}")
+    file_full_name = to_dir + "distr_{}_{}_{}.png".format(tag, feature_group_name, num_src)
+    plt.savefig(file_full_name)
+    print(f"[INFO] save fig to {file_full_name}")
+    # plt.show()
