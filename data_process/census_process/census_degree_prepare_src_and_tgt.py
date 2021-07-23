057fe71a3f4bf43c7f310b35ff3bc9855280c78a
yankang <kangyan2003@gmail.com>
2021-07-23 10:14:26 +0800

    minor update

--- data_process/census_process/census_degree_prepare_src_and_tgt.py
+++ data_process/census_process/census_degree_prepare_src_and_tgt.py
@@ -4,8 +4,12 @@ from data_process import data_process_utils
 
 if __name__ == "__main__":
     data_dir = "/Users/yankang/Documents/Data/census/output/"
-    source_train_file_name = data_dir + 'undergrad_census9495_da_300_train.csv'
-    target_train_file_name = data_dir + 'grad_census9495_da_300_train.csv'
+
+    # tag = "all4000pos004v4"
+    # tag = "all4000pos002"
+    tag = 'all4000pos001'
+    source_train_file_name = data_dir + f'undergrad_census9495_da_{tag}_train.csv'
+    target_train_file_name = data_dir + f'grad_census9495_ft_{tag}_train.csv'
 
     df_src_data = pd.read_csv(source_train_file_name, skipinitialspace=True)
     df_tgt_data = pd.read_csv(target_train_file_name, skipinitialspace=True)
@@ -15,6 +19,6 @@ if __name__ == "__main__":
     df_data = data_process_utils.combine_src_tgt_data(df_src_data, df_tgt_data)
     print("[INFO] df_src_tgt_data shape:", df_data.shape)
 
-    file_full_name = "{}/degree_src_tgt_census9495_da_300_train.csv".format(data_dir)
+    file_full_name = "{}/degree_src_tgt_census9495_da_{}_train.csv".format(data_dir, tag)
 
     data_process_utils.save_df_data(df_data, file_full_name)
