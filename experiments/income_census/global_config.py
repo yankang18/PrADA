219ba88aed736d4f43169f70879236c732d73eab
yankang <kangyan2003@gmail.com>
2021-07-23 13:47:29 +0800

    minor update

--- experiments/income_census/global_config.py
+++ experiments/income_census/global_config.py
@@ -1,9 +1,4 @@
-# feature_extractor_architecture_list = [[28, 40, 28, 6],
-#                                        [25, 40, 25, 6],
-#                                        [36, 52, 36, 8],
-#                                        [27, 40, 27, 6],
-                                       # [20, 36, 20, 6]
-                                       # ]
+
 # feature_extractor_architecture_list = [[28, 56, 28, 14],
 #                                        [25, 50, 25, 12],
 #                                        [36, 72, 36, 18],
@@ -13,14 +8,30 @@
 feature_extractor_architecture_list = [[28, 56, 28, 14],
                                        [25, 50, 25, 12],
                                        [56, 86, 56, 18],
-                                       [27, 54, 27, 13]
-                                       ]
+                                       [27, 54, 27, 13]]
+
+# feature_extractor_for_intr_architecture_list = [[53, 106, 53, 26],
+#                                                 [84, 142, 84, 32],
+#                                                 [55, 110, 55, 27],
+#                                                 [81, 136, 81, 30],
+#                                                 [52, 104, 52, 25],
+#                                                 [83, 140, 83, 31]]
+
+feature_extractor_for_intr_architecture_list = [[53, 78, 53, 15],
+                                                [84, 120, 84, 20],
+                                                [55, 81, 55, 15],
+                                                [81, 120, 81, 20],
+                                                [52, 78, 52, 15],
+                                                [83, 120, 83, 20]]
 
 pre_train_dann_hypterparameters = {
-    "lr": 5e-4,
-    "batch_size": 64,
+    "lr": 6e-4,
+    "batch_size": 128,
     "apply_global_domain_adaption": False,
     "global_domain_adaption_lambda": 1.0,
-    "pos_class_weight": 5,
-    "epoch_patience": 1.5,
-    "valid_metric": ('ks', 'auc')}
+    "pos_class_weight": 1.0,
+    "epoch_patience": 2,
+    "valid_metric": ('ks', 'auc'),
+    # "data_tag": "all4000pos004v4"
+    "data_tag": "all4000pos002"
+}
